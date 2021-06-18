4d167c79725c1f4ee20536bcbc2943f7e0c81a91
Sumit Singh <47050448+Neinun@users.noreply.github.com>
2021-06-18 18:28:22 +0530

    Create leetcode#1480.cpp

---
+++ leetcode#1480.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int temp = 0;
+        
+        for(int i =0; i<nums.size(); i++)
+        {
+            nums[i] = nums[i] + temp;
+            temp = nums[i];
+        }
+        return nums;
+    }
+};
