f9c92c50f331f7321d90d5f81ac458c0be93a417
Sumit Singh <47050448+Neinun@users.noreply.github.com>
2021-06-18 19:00:51 +0530

    Create leetcode#33.cpp

---
+++ leetcode#33.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int n = nums.size();
+        int end = n-1;
+        int mid,prev,next;
+        int result;
+        
+        while(start<=end)
+        {
+            mid = start + (end-start)/2;
+            next = (mid + 1) % n;
+            prev = (mid + n -1) % n;
+            if(nums[mid]<nums[prev] && nums[mid]<nums[next])
+            {
+                result = mid;
+                break;
+            }
+            else if(nums[start]<nums[mid])
+                start = start + 1;
+             else if(nums[mid]<nums[end])
+                end = mid-1;
+            else 
+                start = start + 1;
+        }
+        start = 0;
+        end = result -1;
+        while(start<=end)
+        {
+            mid = start + (end-start)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            if(nums[mid]>target)
+            {
+                end = mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                start = mid + 1;
+            }
+        }
+        start = result;
+        end = n-1;
+        while(start<=end)
+        {
+            mid = start + (end-start)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            if(nums[mid]>target)
+            {
+                end = mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                start = mid + 1;
+            }
+        }
+        return -1;
+        
+    }
+};
